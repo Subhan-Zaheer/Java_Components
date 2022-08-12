6dc761f6e274c503e8b9d82f6517e7386f0cc62b
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-12 22:31:44 +0500

    Repo Started

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java!");
+
+        // Variables:
+        // Variables Name can start from $ or _ .
+        // Java is case-sensitive language.
+
+        int $a = 12;
+        float b = 12f;
+        String name = "Subhan";
+        boolean isMan = true;
+        System.out.println($a);
+        System.out.println(name);
+        System.out.println(b);
+        System.out.println(isMan);
+    }
+}

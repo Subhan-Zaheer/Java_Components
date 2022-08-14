ed618efabaec9704f7ac72fb85e79d8ea8b9c254
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-14 22:59:29 +0500

    If-elseif implemented

--- Conditional_Statements.java
+++ Conditional_Statements.java
@@ -7,11 +7,24 @@ public class Conditional_Statements {
         System.out.print("Enter your age : ");
         int age = scan.nextInt();
 
-        if (age > 18){
+        if (age >= 18){
             System.out.println("You are eligible for National Identity card.");
         }
         else{
             System.out.println("You are not eligible for National Identity card.");
+        }
+
+        System.out.print("Enter your weight : ");
+        int weight = scan.nextInt();
+
+        if(weight < 50){
+            System.out.println("You are UNDER WEIGHT.");
+        }
+        else if (weight >= 50 && weight < 100 ){
+            System.out.println("You are HEALTHY PERSON as long as weight is concerned.");
+        }
+        else {
+            System.out.println("You are OVER WEIGHT.");
         }
     }
 

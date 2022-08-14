29e02a67b7020db12b64585d61f5f02ad24fdf15
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-14 23:05:00 +0500

    Switch case implemented

--- Conditional_Statements.java
+++ Conditional_Statements.java
@@ -26,6 +26,20 @@ public class Conditional_Statements {
         else {
             System.out.println("You are OVER WEIGHT.");
         }
+
+        // Switch Statement
+
+        switch (age){
+            case 12:
+                System.out.println("You are 12 years old.");
+                break;
+            case 56:
+                System.out.println("You are 56 years old.");
+                break;
+            default:
+                System.out.println("You didn't match any case.");
+                break;
+        }
     }
 
 }

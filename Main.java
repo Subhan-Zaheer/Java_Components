05b5a7b316c6a2ad0c57d26752027d5d58892f86
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-08-14 22:43:27 +0500

    Math library examples

--- Main.java
+++ Main.java
@@ -3,44 +3,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Java!");
+//        System.out.println("Hello Java!");
+//
+//        // Variables:
+//        // Variables Name can start from $ or _ .
+//        // Java is case-sensitive language.
+//
+//        int $a = 12;
+//        float b = 12f;
+//        String name = "Subhan";
+//        boolean isMan = true;
+//        System.out.println($a);
+//        System.out.println(name);
+//        System.out.println(b);
+//        System.out.println(isMan);
+//        /*
+//         Primitive Data types : byte (1 byte), char (2 byte), int (4 byte), float (4 byte), long (8 byte), double (8 byte)
+//         Non-Primitive Data types : Objects of classes that user create by himself.
+//         */
+//
+//        // Operators in Java:
+//        // Operand - Operator - Operation
+//        /*
+//        Types of Operators in Java :
+//        Arithematic - Common Mathematical operations. + - * / % ++ --
+//        Assignment - = += -= *= /= %=
+//        Comparison - == != < > <= >=
+//        Logical - &&(and) ||(or) !(not)
+//         */
+//        System.out.println(45 % 78);
+//        // Taking input from user in java.
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter a number : ");
+//        String input = scan.nextLine();
+//        System.out.println(input);
+//        // Sting Methods
+//        // You can access all methods of String using '.' operator.
+//        String str = "Subhan";
+//        System.out.println(str.intern());
+//        // Conditional Statements in Java.
 
-        // Variables:
-        // Variables Name can start from $ or _ .
-        // Java is case-sensitive language.
+        // Math Library
+        
+        int num1 = 6, num2 = 9;
+        System.out.println(Math.random());
+        // If we want to generate a number between 4 and 8.
+        System.out.println((4 + (8-4)*Math.random()));
 
-        int $a = 12;
-        float b = 12f;
-        String name = "Subhan";
-        boolean isMan = true;
-        System.out.println($a);
-        System.out.println(name);
-        System.out.println(b);
-        System.out.println(isMan);
-        /*
-         Primitive Data types : byte (1 byte), char (2 byte), int (4 byte), float (4 byte), long (8 byte), double (8 byte)
-         Non-Primitive Data types : Objects of classes that user create by himself.
-         */
 
-        // Operators in Java:
-        // Operand - Operator - Operation
-        /*
-        Types of Operators in Java :
-        Arithematic - Common Mathematical operations. + - * / % ++ --
-        Assignment - = += -= *= /= %=
-        Comparison - == != < > <= >=
-        Logical - &&(and) ||(or) !(not)
-         */
-        System.out.println(45 % 78);
-        // Taking input from user in java.
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number : ");
-        String input = scan.nextLine();
-        System.out.println(input);
-        // Sting Methods
-        // You can access all methods of String using '.' operator.
-        String str = "Subhan";
-        System.out.println(str.intern());
-        // Conditional Statements in Java.
     }
 }
